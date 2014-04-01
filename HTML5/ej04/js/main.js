d17b68dc1eceec3b53d5044aeadd5778a7aff78f
mRomanSalpico <salorino@gmail.com>
2014-04-01 18:59:30 +0200

    ejercicios HTML5

--- HTML5/ej04/js/main.js
+++ HTML5/ej04/js/main.js
@@ -12,12 +12,20 @@ $(function(){
 
 
     //Sacar por pantalla que inputs reconoce el navegador
-
+console.log(Modernizr);
     var $ul = $('#datos');
     $ul.empty();
+    $ul.append('<li>TIPOS: </li>');
+
     for (var tipo in Modernizr.inputtypes){
 
         $ul.append('<li>Tipo: ' + tipo + ' ----  Valor: ' + Modernizr.inputtypes[tipo] +'</li>');
+
+    }
+    $ul.append('<li>VIDEO: </li>');
+    for (var codec in Modernizr.video){
+
+        $ul.append('<li>Codec: ' + codec + ' ----  Valor: ' + Modernizr.video[codec] +'</li>');
 
     }
 
